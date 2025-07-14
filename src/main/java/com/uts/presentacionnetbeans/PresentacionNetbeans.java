a6d429a3270f556f38b3d96234f32a2d8b53ae9f
luigui29 <luiguigarrido29@gmail.com>
2025-07-13 23:11:54 -0400

    initial commit

---
+++ src/main/java/com/uts/presentacionnetbeans/PresentacionNetbeans.java
@@ -0,0 +1,34 @@
+package com.uts.presentacionnetbeans;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.BorderFactory;
+
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import javax.swing.ImageIcon;
+
+public class PresentacionNetbeans {
+    
+    static void GUI() {
+        ImageIcon gif = new ImageIcon("C:\\Users\\luigu\\Documents\\NetBeansProjects\\PresentacionNetbeans\\src\\main\\java\\com\\uts\\presentacionnetbeans\\slowpoke.gif");
+        
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel(gif);
+        
+        panel.setBorder(BorderFactory.createEmptyBorder(200, 200, 200, 200));
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(label);
+        
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Espero Estar Haciendo Esto Bien");
+        frame.pack();
+        frame.setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        GUI();
+    }
+}
